6d1bb7d7d13f11917a17fae5465a11cebad1161f
Nelly20 <nelly.turak@gmail.com>
2020-07-05 19:47:54 +0300

    Завершаем вёрстку

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-var popup = document.querySelector(".write-us");
-popup.classList.add("modal-show");
+var link = document.querySelector(".modal-link");
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	name.focus();
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+	evt.preventDefault();
+	console.log(name.value);
+	console.log(email.value);
+});
